898b9574aa8fb4ab37e95950adef320f364e3be3
Kristian Sakarisson <kristian@sakarisson.com>
2016-06-08 20:04:25 +0100

    Added stuff from tonight

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    GameObject player;
+    public float moveSpeed;
+
+    bool isGrounded = false;
+
+    Vector3 moveVector; 
+
+	void Start ()
+    {
+        player = gameObject;
+        moveVector = new Vector3(moveSpeed, 0, 0);
+    }
+	
+	void Update ()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+            player.GetComponent<Transform>().position = player.GetComponent<Transform>().position + moveVector / 20;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            player.GetComponent<Transform>().position = player.GetComponent<Transform>().position - moveVector / 20;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            isGrounded = false;
+            player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnTriggerStay2D (Collider2D other)
+    {
+        if(other.name == "platform")
+            isGrounded = true;
+    }
+}
